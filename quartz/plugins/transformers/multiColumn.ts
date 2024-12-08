e05b3abe9bb62633e2a629e9edf3c8a0d26a2c77
K. Kraken S. Nielsen <kraken122@live.dk>
2024-12-08 14:58:50 +0100

    Update multiColumn.ts

--- quartz/plugins/transformers/multiColumn.ts
+++ quartz/plugins/transformers/multiColumn.ts
@@ -26,7 +26,7 @@ function parseColumnSettings(text: string): Record<string, string> {
   return settings
 }
 
-export const function MultiColumn(userOptions: Partial<MultiColumnOptions> = {}): QuartzTransformerPlugin {
+export default function MultiColumn(userOptions: Partial<MultiColumnOptions> = {}): QuartzTransformerPlugin {
   const options = { ...defaultOptions, ...userOptions }
 
   return {
