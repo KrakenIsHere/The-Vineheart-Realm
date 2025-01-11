01486b0ee11188e681cd17ad74827ad18abe257e
K. Kraken S. Nielsen <kraken122@live.dk>
2025-01-11 12:07:01 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "tvr.krakeenia.com",
-    ignorePatterns: ["**/DM*", "private", "templates", ".obsidian"],
+    ignorePatterns: ["**/DM*", "private", "templates", ".obsidian" "/Chats"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
